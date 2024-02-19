65a27d2f718fffcf2a6c64d4d9e4294e2f341610
Dayane <dayanetm@outlook.com>
2024-02-19 12:49:13 -0300

    logic with python

---
+++ ordenacao_dicionario.py
@@ -0,0 +1,8 @@
+# Primeiro, execute a atribuição palavras = ['taco', 'bola', 'celeiro', 'cesta', 'peteca'] 
+# Agora, escreva duas expressões Python que são avaliadas, respectivamente, como a primeira e a última palavras em palavras, na ordem do dicionário
+
+words = ['taco', 'bola', 'celeiro', 'cesta', 'peteca']
+
+ordered_words = sorted(words)
+
+print("Ordered words: ", ordered_words)
